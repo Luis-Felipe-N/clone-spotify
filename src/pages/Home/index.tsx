e658c6e67a732aaa11dcc2aa75bfc610c3a5349e
Luis-Felipe-N <luisj2felipe09@gmail.com>
2021-10-25 20:02:59 -0300

    first commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import { Footer } from '../../components/Footer'
+import { Header } from '../../components/Header'
+import styles from './styles.module.scss'
+
+ export function Home() {
+    return (
+        <>
+        <Header />
+            <main className={ styles.homeContainer }>
+                oi
+            </main>
+        <Footer />
+        </>
+    )
+}
