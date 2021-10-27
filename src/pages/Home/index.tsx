9fc22a711ea2361080a34f21034f0daeed29dd89
Luis-Felipe-N <luisj2felipe09@gmail.com>
2021-10-26 23:34:38 -0300

    started menu mobile

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,31 @@
 import styles from './styles.module.scss'
 import { Footer } from '../../components/Footer'
 import { Header } from '../../components/Header'
+import { useState } from 'react'
 
  export function Home() {
+    const [ user, setUser ] = useState(true)
+
     return (
         <>
         <Header />
-            <main className={ styles.homeContainer }>
-                <div>
-                    <h1>Suas músicas estão com saudade</h1>
-                    <p>Continue curtindo de onde parou.</p>
-                    <a href="#">Acesse o web player</a>
-                </div>
-            </main>
+           { user ? (
+                <main className={ styles.homeContainerLogged }>
+                    <div>
+                        <h1>Suas músicas estão com saudade</h1>
+                        <p>Continue curtindo de onde parou.</p>
+                        <a href="#">Acesse o web player</a>
+                    </div>
+                </main>
+           ) : (
+                <main className={ styles.homeContainer }>
+                    <div>
+                        <h1     >Escutar muda tudo</h1>
+                        <p>Milhões de músicas e podcasts para explorar. E nem precisa de cartão de crédito.</p>
+                        <a href="#">Acesse o web player</a>
+                    </div>
+                </main>
+           )}
         <Footer />
         </>
     )
